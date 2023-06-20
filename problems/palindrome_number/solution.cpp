3e2f36958449ee8d5a65760d621487d3fe0e1bd7
Elon Hadad <101451973+elonhadad@users.noreply.github.com>
2023-06-20 22:29:53 +0000

    Sync LeetCode submission - Palindrome Number (cpp)

---
+++ problems/palindrome_number/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+            int n = x;
+            long rev = 0;
+            while (n != 0) {
+            rev = 10 * rev + (n % 10);
+            n = n / 10;
+    }
+    return (rev == x && x >= 0);
+    }
+};
